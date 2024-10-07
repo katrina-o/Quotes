e3130ca780d88686c583eecd34b7d5200e1c7667
katrina-o <ekaterinaorlova25041994@gmail.com>
2024-10-07 11:11:21 +0300

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Катя on 07.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
